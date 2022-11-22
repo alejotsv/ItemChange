8ff0fbddd24644817a584a238f2f6e62f08f490c
Alejandro Salas <asalas.wd@outlook.com>
2022-11-21 19:56:36 -0500

    Get text from radio button

--- src/com/java21days/ItemChange.java
+++ src/com/java21days/ItemChange.java
@@ -8,6 +8,7 @@ import java.awt.event.ItemListener;
 public class ItemChange extends JFrame implements ItemListener {
     Dimension dim = new Dimension(400, 100);
     JPanel panel;
+    ButtonGroup group = new ButtonGroup();
 
     ItemChange(String title, String[] buttons){
         super(title);
@@ -18,21 +19,24 @@ public class ItemChange extends JFrame implements ItemListener {
         addButtons(panel, buttons);
         FlowLayout flow = new FlowLayout();
         panel.setLayout(flow);
+
         add(panel);
         setVisible(true);
     }
 
     @Override
     public void itemStateChanged(ItemEvent itemEvent) {
-        System.out.println("Item changed");
+//        String text = getButtonText(itemEvent.getSource().toString());
+        String text = ((JRadioButton)itemEvent.getItem()).getText();
+        System.out.println(text);
+        System.out.println(itemEvent.getStateChange());
     }
 
     void addButtons(JPanel panel, String[] buttons){
-        ButtonGroup group = new ButtonGroup();
         for( String btn : buttons ){
-            System.out.println(btn);
             JRadioButton radioBtn = new JRadioButton(btn);
-            group.add(radioBtn);
+            radioBtn.addItemListener(this);
+            this.group.add(radioBtn);
             panel.add(radioBtn);
         }
     }
