0f18ce67bbb07f09ba919a809250cf86e0753674
Alejandro Salas <asalas.wd@outlook.com>
2022-11-21 20:19:26 -0500

    Add label dependent on option selected

--- src/com/java21days/ItemChange.java
+++ src/com/java21days/ItemChange.java
@@ -9,32 +9,40 @@ public class ItemChange extends JFrame implements ItemListener {
     Dimension dim = new Dimension(400, 100);
     JPanel panel;
     ButtonGroup group = new ButtonGroup();
+    JLabel label;
 
     ItemChange(String title, String[] buttons){
         super(title);
         setSize(dim);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         addButtons(panel, buttons);
+        label = new JLabel("You think Argentina will win the World Cup");
+        panel.add(label);
         FlowLayout flow = new FlowLayout();
         panel.setLayout(flow);
-
         add(panel);
         setVisible(true);
     }
 
     @Override
     public void itemStateChanged(ItemEvent itemEvent) {
-//        String text = getButtonText(itemEvent.getSource().toString());
-        String text = ((JRadioButton)itemEvent.getItem()).getText();
-        System.out.println(text);
-        System.out.println(itemEvent.getStateChange());
+        if(itemEvent.getStateChange() == 1){
+            String text = ((JRadioButton)itemEvent.getItem()).getText();
+            label.setText("You think " + text + " will win the World Cup");
+        }
     }
 
     void addButtons(JPanel panel, String[] buttons){
+        int i = 0;
         for( String btn : buttons ){
-            JRadioButton radioBtn = new JRadioButton(btn);
+            JRadioButton radioBtn;
+            if (i==0){
+                radioBtn = new JRadioButton(btn, true);
+            } else {
+                radioBtn = new JRadioButton(btn);
+            }
             radioBtn.addItemListener(this);
             this.group.add(radioBtn);
             panel.add(radioBtn);
